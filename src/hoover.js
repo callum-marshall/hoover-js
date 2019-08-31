3bb4894ec2da7cd6c6a54bfff45729acc5858fb4
callum-marshall <callum_marshall@hotmail.co.uk>
2019-08-31 15:01:24 +0100

    add currentLocation

--- src/hoover.js
+++ src/hoover.js
@@ -4,6 +4,7 @@ class Hoover {
   constructor (filename) {
     this.input = fs.readFileSync(`${filename}`, 'utf8').split(/\n/)
     this.roomSize = this.input[0].split(' ').map(Number)
+    this.currentLocation = this.input[1].split(' ').map(Number)
   }
 }
 
