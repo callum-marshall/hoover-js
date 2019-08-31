e9546f34ebae146842174db833c2e26fa9fdf299
callum-marshall <callum_marshall@hotmail.co.uk>
2019-08-31 14:57:24 +0100

    add roomSize

--- spec/hoover.spec.js
+++ spec/hoover.spec.js
@@ -3,8 +3,15 @@ const { Hoover } = require('../src/hoover')
 describe('Hoover', () => {
   let hoover
 
-  test('reads the text file and saves it as an array of strings', () => {
+  beforeEach(() => {
     hoover = new Hoover('./spec/TestFile.txt')
+  })
+
+  test('reads the text file and saves it as an array of strings', () => {
     expect(hoover.input).toEqual(['5 5', '1 1', '2 2', 'NESW'])
+  })
+
+  test('stores room size as coordinates', () => {
+    expect(hoover.roomSize).toEqual([5, 5])
   })
 })
--- src/hoover.js
+++ src/hoover.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 class Hoover {
   constructor (filename) {
     this.input = fs.readFileSync(`${filename}`, 'utf8').split(/\n/)
+    this.roomSize = this.input[0].split(' ').map(Number)
   }
 }
 
